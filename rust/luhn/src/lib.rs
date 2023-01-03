f0bdd7f249c3324e353e584b1f81fcffe9061f1b
Fabian Bergström <fabian.bergstrom@kivra.com>
2023-01-03 16:13:00 +0100

    use rustler for implementing the nif

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,14 +1,14 @@
+extern crate rustler;
+
+use rustler::{Env, Term};
+
+pub fn on_load(_env: Env, _load_info: Term) -> bool {
+    true
+}
+
+#[rustler::nif]
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+rustler::init!("luhn", [add], load = on_load);
