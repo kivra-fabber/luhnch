90ead8fc86f408157318cfca50c67b3c4f587cd0
Fabian Bergström <fabian.bergstrom@kivra.com>
2023-01-11 10:02:15 +0100

    Fix copyright notices

--- src/luhn.erl
+++ src/luhn.erl
@@ -1,4 +1,4 @@
-%%% Copyright 2023, Fabian Bergström <fabian.bergstrom@kivra.com>.
+%%% Copyright 2023, Kivra AB
 %%%
 %%% Licensed under the Apache License, Version 2.0 (the "License");
 %%% you may not use this file except in compliance with the License.
